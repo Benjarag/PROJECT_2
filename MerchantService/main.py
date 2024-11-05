40c178665baeab9623cc6c76580653ebd82d6152
sindri-afk <sindribjarka@gmail.com>
2024-11-05 13:43:14 +0000

    Merchant Service almost complete
    hamingju hristingur á móti,.

--- MerchantService/main.py
+++ MerchantService/main.py
@@ -1,38 +1,68 @@
-from models.model import Merchant
-import uuid
+from MerchantService.model import Merchant
 import uvicorn
-from typing import Dict
+from typing import Dict, List
 from fastapi import FastAPI, HTTPException, status
 import json 
 
 
 app = FastAPI()
 
-def merchant_database(file_path: str):
+merchant = Merchant()
+
+DATABASE_FILE = "MerchantService/MerchantDB.json"
+
+def read_merchants(file_path: str):
+
     with open(file_path, 'r') as f:
         return json.load(f)
 
-#temp database 
-merchants_db: Dict[str, dict] = {}
+def save_merchants(merchants: List[Dict]) -> None:
 
-class MerhcantResponse(Merchant):
-    id: str
+    with open (DATABASE_FILE, 'w') as data_file:
+        json.dump(merchants, data_file)
+
+
+#temp database
+merchants_db = {merchant["id"]: merchant for merchant in read_merchants(DATABASE_FILE)}
+
+
 @app.post("/merchant/", response_model=Merchant, status_code=status.HTTP_201_CREATED)
 async def create_merchant(merchant: Merchant):
-    merchant_id = str(uuid.uuid4())
 
-    merchants_db[merchant_id] = merchant.model_dump()
+    if merchants_db: 
 
-    return {"id": merchant_id, **merchant.model_dump()}
+        previous_id = max(merchant["id"] for merchant in merchants_db.values()) # stærsta id er væntanlega seinasta id sem var assignað til merchant.
+    else:
 
-@app.get("/merchant/{merchant_id}", response_model=Merchant)
-async def get_merchant(id: str):
-    merchant = merchants_db.get(id)
+        previous_id = 0
+
+    merchant_id = 1 + previous_id
+
+    new_merchant = Merchant(id=merchant_id, **merchant.model_dump())
+
+    merchants_db[merchant_id] = new_merchant
+
+    save_merchants(list(merchants_db.values()))
+
+    return new_merchant
+
+@app.get("/merchants/{merchant_id}", response_model=Merchant)
+async def get_merchant(merchant_id: int):
+
+    merchant = merchants_db.get(merchant_id)
 
     if merchant is None:
         raise HTTPException(status_code=404, detail="Merchant not found")
-    return {"id": id, **merchant}
-
+    
+    return {
+        "name" : merchant["name"],
+        "ssn" : merchant["ssn"],   
+        "email" : merchant["email"],
+        "phone_number" : merchant["phone_number"],
+        "allows_discount" : merchant["allows_discount"]
+    }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
+
+
--- MerchantService/model.py
+++ MerchantService/model.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
 class Merchant(BaseModel):
+    id: int 
     name: str
     ssn: str
     email: str
