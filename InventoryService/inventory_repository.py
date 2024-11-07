1d1b74fa53b3f8502da1217e657bd3ad934895d9
olliemil <oliverkjaran@gmail.com>
2024-11-07 21:56:35 +0000

    Added put functionality to inventoryservice

--- InventoryService/inventory_repository.py
+++ InventoryService/inventory_repository.py
@@ -39,13 +39,16 @@ class InventoryRepository:
             products = json.load(file)
             return products.get(product_id)  # Directly access by integer ID
 
-    def update_product(self, product_id: int, merchant_id: int, product_name: str, price: float, quantity: int) -> int:
+    def update_product(self, product_id: int) -> int:
         # Load existing products
         with open(self.file_path, 'r+') as file:
             products = json.load(file)
-        products[product_id]["quantity"] -= 1
-        products[product_id]["reserved"] += 1
-        file.seek(0)
+            products[product_id]["quantity"] -= 1
+            products[product_id]["reserved"] += 1
+            file.seek(0)
+            json.dump(products, file, indent=4)
+            file.truncate()
+        return product_id, products.get(product_id)
 
 
 
